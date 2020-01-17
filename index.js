d6b781a4586dbe3ce2e3daa762d5db3a2d151692
Tripple-A <biodun9@gmail.com>
2020-01-17 22:51:03 +0100

    added display function for weather data

--- index.js
+++ index.js
@@ -12,15 +12,25 @@ async function findWeather(url) {
 const locationUrl = function getUrl() {
   const input = document.querySelector('input');
   const url = `http://api.openweathermap.org/data/2.5/weather?q=${input.value}&APPID=7c933702219e1d26960b3cc11edb864c`;
+  input.value = '';
   return url;
 };
 
+function assign(a, b) {
+  document.getElementById(a).textContent = b;
+  document.querySelector('.display').style.display = 'block'
+}
+
 function get(data) {
-  console.log(data)
+  console.log(data);
   const temp = data.main.temp;
-  const desc = data.weather.description;
+  const desc = data.weather[0].description;
   const country = data.sys.country;
-  const town = data.name
+  const town = data.name;
+  assign('temp', temp);
+  assign('desc', desc);
+  assign('country', country);
+  assign('town', town);
 }
 
 const btn = document.querySelector('button');
