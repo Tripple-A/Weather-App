193f1a3b98d32f6261d82bff8c69d9f993230db7
Tripple-A <biodun9@gmail.com>
2020-01-20 19:23:48 +0100

    edited eslintrc.json

--- src/index.js
+++ src/index.js
@@ -1,87 +1,73 @@
 import './index.css';
-import { celcius, farenheit } from './temp';
+import { farenheit } from './temp';
+import { findWeather, find } from './logic';
 
-const giphy = require('giphy-api')('1ciWQ3R3BbnLWyjnSWHUIKmeC1TMV1wH');
 
-const temp = document.getElementById('temp');
 const deg = document.getElementById('degrees');
 const btn = document.getElementById('check');
 const btn2 = document.getElementById('far');
-const btn3 = document.getElementById('cel');
 const input = document.querySelector('input');
 const progress = document.querySelector('.lds-roller');
 
-async function find(search) {
-  try {
-    const response = await giphy.search(search);
-    document.querySelector('img').src = response.data[0].images.original.url;
-    return response.data[0].images.original.url;
-  } catch (err) {
-    return err;
-  }
-}
 
-function assign(a, b) {
+const assign = (a, b) => {
   document.getElementById(a).textContent = b;
   document.querySelector('.display').style.display = 'block';
   document.getElementById('date').style.display = 'none';
-}
+};
 
-
-function get(data) {
-  const { temp } = data.main;
-  const desc = data.weather[0].description;
-  document.querySelector('img').src = find(desc);
-  const { country } = data.sys;
-  const town = data.name;
-  deg.textContent = 'C';
-  assign('temp', Math.round(parseFloat(temp) - 273.15));
-  assign('desc', desc);
-  assign('country', country);
-  assign('town', town);
-  progress.style.display = 'none';
-}
-
-function show() {
+const show = () => {
   document.querySelector('.display').style.display = 'none';
   document.getElementById('date').style.display = 'block';
   progress.style.display = 'none';
-}
+};
 
-async function findWeather(url) {
-  let responseData;
-  try {
-    const response = await fetch(url, { mode: 'cors' });
-    responseData = await response.json();
-    get(responseData);
-  } catch (err) {
-    assign('date', 'We could not find the weather for your specified location');
-    show();
+const unshow = () => {
+  progress.style.display = 'block';
+};
+
+
+const get = (data) => {
+  if (data === '204') { show(); } else {
+    assign('desc', data.description);
+    assign('country', data.country);
+    assign('town', data.name);
+    progress.style.display = 'none';
   }
-  return responseData;
-}
+};
 
-const locationUrl = function getUrl() {
+const locationUrl = () => {
   const url = `http://api.openweathermap.org/data/2.5/weather?q=${input.value}&APPID=7c933702219e1d26960b3cc11edb864c`;
   return url;
 };
 
+
+async function check1() {
+  const data = await findWeather(locationUrl());
+  const url = await find(data.description);
+  const temp = Math.round(parseFloat(data.temp) - 273.15);
+  assign('temp', temp);
+  document.querySelector('img').src = url;
+  get(data);
+}
+
+async function check2() {
+  const data = await findWeather(locationUrl());
+  const url = await find(data.description);
+  const temp = Math.round(parseFloat(data.temp) - 273.15);
+  assign('temp', farenheit(temp));
+  document.querySelector('img').src = url;
+  get(data);
+}
+
 btn.addEventListener('click', () => {
-  findWeather(locationUrl());
-  input.value = '';
-  progress.style.display = 'block';
+  check1();
+  deg.textContent = 'C';
+  unshow();
 });
 
 btn2.addEventListener('click', () => {
-  if (deg.textContent === 'C') {
-    temp.textContent = farenheit(temp.textContent);
-    deg.textContent = 'F';
-  }
-});
-
-btn3.addEventListener('click', () => {
-  if (deg.textContent === 'F') {
-    temp.textContent = celcius(temp.textContent);
-    deg.textContent = 'C';
-  }
+  check2();
+  deg.textContent = 'F';
+  unshow();
 });
--- src/logic.js
+++ src/logic.js
@@ -1,8 +1,31 @@
+const giphy = require('giphy-api')('1ciWQ3R3BbnLWyjnSWHUIKmeC1TMV1wH');
 
-    var twoSum = function(nums, target) {
-        arr =[]
-        //let i = 1;
-arr.push(target)
-return arr
+async function find(search) {
+  try {
+    const response = await giphy.search(search);
+    return response.data[0].images.original.url;
+  } catch (err) {
+    return err;
+  }
+}
+
+async function findWeather(url) {
+  let responseData;
+  let weather;
+  try {
+    const response = await fetch(url, { mode: 'cors' });
+    responseData = await response.json();
+    const { temp } = responseData.main;
+    const { description } = responseData.weather[0];
+    const { country } = responseData.sys;
+    const { name } = responseData;
+    weather = {
+      temp, description, country, name,
     };
-    twoSum([2,7,11,15],9)
+  } catch (err) {
+    return '204';
+  }
+  return weather;
+}
+
+export { findWeather, find };
