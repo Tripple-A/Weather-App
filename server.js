1e5d04a7d12ef131cf18a42fef200b9b07048e91
Tripple-A <biodun9@gmail.com>
2020-02-22 23:56:01 +0100

    express added

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 
 const path = require('path');
-const port = process.env.PORT || 8085;
+const port = process.env.PORT || 8080;
 const app = express();
 
 // the __dirname is the current directory from where the script is running
